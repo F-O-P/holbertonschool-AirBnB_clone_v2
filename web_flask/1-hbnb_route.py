23718463f2e5d0ac3ed30b966fb97f4a8182d4db
tayler.coon@gmail.com <tayler.coon@gmail.com>
2023-07-13 12:35:41 -0700

    fixed pycode styling

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Starts a flask web app that returns a greeting """
 
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -11,10 +12,12 @@ def greeting():
     """ function that returns greeting """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """function that return message"""
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
