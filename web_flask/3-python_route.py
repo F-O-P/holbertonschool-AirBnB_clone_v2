2c2ed8eec348b3386040535320cad69d57293861
tayler.coon@gmail.com <tayler.coon@gmail.com>
2023-07-13 12:53:26 -0700

    thissss is task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Starts a flask web app that returns a greeting """
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def greeting():
+    """ function that returns greeting """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ function that returns a hbnb message """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ function that return message, but with a C """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/<hiss>', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
+def parseltongue(hiss="is cool"):
+    """ function that returnsss a python message """
+    return 'Python {}'.format(hiss.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
